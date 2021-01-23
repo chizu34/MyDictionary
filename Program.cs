c27368c343d2d001830fe060dfbbeef9c93a436e
semih <semih@DESKTOP-6EF30B5>
2021-01-23 21:23:24 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> liste = new MyList<string>();
+            liste.Add("Semih");
+            liste.Add("Engin");
+            Console.WriteLine(liste.Length);
+
+            foreach (var isimler in liste.Items)
+            {
+                Console.WriteLine(isimler);
+            }
+        }
+    }
+}
